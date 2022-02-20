a2b4e1bc22bc85ea3e69427395fc199ad23ce6b5
Hack Live <59341206+hack-live@users.noreply.github.com>
2022-02-20 12:54:07 -0800

    Add files via upload

---
+++ vendor.27e4ff72.js
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<link rel="icon" href="/favicon.ico" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta name="description" content="Blockchain: Gamified - Games with real, player-owned economies will become places where we live, work, and play – True digital nations" />
+<title>Sky Mavis</title>
+<meta property="og:type" content="website" />
+<meta property="og:title" content="Sky Mavis" />
+<meta property="og:description" content="Blockchain: Gamified - We build games with player owned economies – true digital nations." />
+<meta property="og:image" content="/thumbnail.png" />
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-4W5DYHM0BL"></script>
+<script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-4W5DYHM0BL');
+    </script>
+<script type="module" crossorigin src="/static/index.7c75d6f1.js"></script>
+<link rel="modulepreload" href="/static/vendor.27e4ff72.js">
+<link rel="stylesheet" href="/static/index.7154b532.css">
+</head>
+<body>
+
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P2L6FMM"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
+<noscript>You need to enable JavaScript to run this app.</noscript>
+</body>
+</html>
